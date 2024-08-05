238fb9e826bee8cb42f42d3c2f238dc36db290af
kunalne <negikunal0325@gmail.com>
2024-08-05 23:58:26 +0530

    New day changes

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -3,7 +3,19 @@ print("This is Second user")
 for i in range(1,99):
     if i%2 == 0:
         print(i*2)
-
+print("********************************************")
 ll = ["sds",34,"sds",67,44,55,67]
 for i in ll:
-    print(ll)
+    print(i)
+
+print("Thanks for the changes man!!!!!!")
+# New code updated
+
+dict ={}
+
+dict["Name"] = "Kunal"
+dict["Age"] = "32"
+dict["Company"] = "Bsoft"
+dict["Hobby"] = "Travelling"
+
+print("New value: ",dict)
