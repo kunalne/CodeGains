aa88e2293a1a97240aab058292024b2fb50f2fa9
kunalne <negikunal0325@gmail.com>
2024-08-05 23:47:18 +0530

    Changes by EST user

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,4 +1,9 @@
 print("This is first user")
+print("This is Second user")
 for i in range(1,99):
     if i%2 == 0:
         print(i*2)
+
+ll = ["sds",34,"sds",67,44,55,67]
+for i in ll:
+    print(ll)
